918ebd891fe870481a7fec28263c78ff9c3ee6c7
wayoflife <dbetsche@gmail.com>
2015-04-21 14:19:51 +0200

    extract frequent renter points

--- src/Rental.java
+++ src/Rental.java
@@ -31,4 +31,13 @@ class Rental {
         }
         return result;
     }
+    
+    public int getFrequentRenterPoints() {
+        // add bonus for a two day new release rental
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
+			return 2;
+		}
+     // add frequent renter points
+        return 1;
+	}
 }
